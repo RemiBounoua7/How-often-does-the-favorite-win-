d580a82174f86cb0d2d94dc439b7cb244eca47dc
RemiBounoua7 <135212777+RemiBounoua7@users.noreply.github.com>
2024-12-16 13:35:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,8 +29,11 @@ function populateTable(data) {
             
 
             // Highlight cells if Method N was right on year X
-            if (colIndex >= 3 && row[2].trim() && td.textContent.includes(row[2].trim())) {
-                td.classList.add('highlight');
+            if (colIndex >= 3 && row[2].trim() && cell === row[0]) {
+                td.classList.add('highlight1');
+            }
+            if (colIndex >= 3 && row[2].trim() && td.textContent.includes(row[2].trim())){
+                td.classList.add('highlight2');
             }
 
             tr.appendChild(td);
